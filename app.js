d63839f8fab425bb1590482c39588287edb78c8f
faysal <devfaysaldb@gmail.com>
2025-01-06 00:19:26 +0600

    some update

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+const express = require('express');
+const app = express();
+
+app.get("/", function(req, res) {
+    res.send("my first backend practice !");
+})
+
+app.get("/test", function(req, res) {
+    res.send("thsi is the test route reload  now then edit this time    ");
+})
+
+app.listen(3000);
