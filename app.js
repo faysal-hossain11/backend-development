a8fbf9820be48d07c37e1611db79b66abafc26eb
faysal <devfaysaldb@gmail.com>
2025-01-12 20:44:03 +0600

    some update

--- app.js
+++ app.js
@@ -1,22 +1,15 @@
-
-
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path')
+const port = 3000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine', 'ejs');
 
-// this is my route
-app.get("/", (req, res, next) => {
-    res.render("index")
-});
-
-app.listen(3000, () => {
-    console.log("server is runnig on port 3000");
-    
- 
+app.get("/", (req, res) => {
+    res.send("hello");
 })
 
+
+
+
+app.listen(port, () => {
+    console.log("server is runnig on localhost:", port);
+})
